c02275ac680c1713978838515ea7d63754ed6cab
Simo <93simonster@gmail.com>
2021-06-27 22:39:11 +0200

    textarea

--- collector/forms.py
+++ collector/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms import Textarea
 from .models import Submission
 
 
@@ -7,3 +8,6 @@ class SubmissionForm(forms.ModelForm):
     class Meta:
         model = Submission
         fields = ('age', 'gender', 'text',)
+        widgets = {
+            'text': Textarea(attrs={'cols': 25, 'rows': 10}),
+        }
